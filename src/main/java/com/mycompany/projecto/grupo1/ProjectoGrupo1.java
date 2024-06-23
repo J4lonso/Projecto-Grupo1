fbcca8886b6b86780b301a8b63a3dada6a307539
J4lonso <j4ms65@gmail.com>
2024-06-22 22:22:19 -0600

    Carga Inicial

---
+++ src/main/java/com/mycompany/projecto/grupo1/ProjectoGrupo1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecto.grupo1;
+
+/**
+ *
+ * @author josea
+ * as
+ * as
+ * assa
+ */
+public class ProjectoGrupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
