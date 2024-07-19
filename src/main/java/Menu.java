2486faacb68c001e697f6a8f608c0d969e027bf7
J4lonso <j4ms65@gmail.com>
2024-07-19 15:30:33 -0600

    Avance del menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,63 @@
+
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+
+    // Constructor de la clase Menu
+    public Menu() {
+        // Puedes inicializar variables aquí si es necesario
+    }
+
+    // Método para mostrar el menú
+    public void mostrarMenu() {
+        while (true) {
+            String menu = "Seleccione una opción:\n"
+                    + "1. Bienvenida\n"
+                    + "2. Información del Zoológico\n"
+                    + "3. Gestión de animales\n"
+                    + "4. Ubicación actual en el Zoológico\n"
+                    + "5. Ubicación de los animales\n"
+                    + "6. Salir\n";
+
+            String opcion = JOptionPane.showInputDialog(menu);
+            if (opcion == null) {
+                JOptionPane.showMessageDialog(null, "¡Hasta luego!");
+                System.exit(0);
+            }
+            int input;
+            try {
+                input = Integer.parseInt(opcion);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "La opción no existe. Por favor ingrese un número del 1 al 6.");
+                continue;
+            }
+
+            switch (input) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Bienvenida");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Información del Zoológico");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Gestión de animales");
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Ubicación actual en el Zoológico");
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Ubicación de los animales");
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null, "Vuelva pronto");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida. Por favor ingrese un número del 1 al 6.");
+                    break;
+            }
+        }
+    }
+}
+
--- src/main/java/Ubicacion_dentro.java
+++ src/main/java/Ubicacion_dentro.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package com.mycompany.projecto.grupo1;
+
 
 /**
  *
  * @author josea
  */
-public class Zoologico {
+public class Ubicacion_dentro {
 
     /**
      * @param args the command line arguments
@@ -16,10 +16,8 @@ public class Zoologico {
     public static void main(String args[]) {
         // TODO code application logic here
         
-        String nombre_zoo = "";
-        String Lugar = "";
-        String Max = "";
-        
+        String nombre_animal = "";
+        String posicion = "";
         
         
     }
